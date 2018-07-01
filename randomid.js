14ff525e7f9b21037bce9cc8fa978a15ff7b6817
chazeprasad <chazeprasad@gmail.com>
2018-07-01 15:44:38 +0530

    initial commit..

---
+++ randomid.js
@@ -0,0 +1,13 @@
+function randomId(length) {
+    let text = ""
+    length = length || 32;
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return text
+}
+
+export default randomId;
